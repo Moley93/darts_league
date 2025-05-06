55f73a6339e48f5061e7bfd6b0fbeed83b87984f
Moley93 <89353994+Moley93@users.noreply.github.com>
2025-05-06 13:01:20 +0100

    schema update

--- schema.sql
+++ schema.sql
@@ -3,9 +3,9 @@
 -- ========================================
 
 -- Create the database
-DROP DATABASE IF EXISTS darts_league;
-CREATE DATABASE darts_league;
-USE darts_league;
+-- DROP DATABASE IF EXISTS darts_league;
+-- CREATE DATABASE darts_league;
+-- USE darts_league;
 
 -- ========================================
 -- TABLE DEFINITIONS
